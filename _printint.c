d989d545f7751a24d3ee9e09ebe9e85979e90b42
slyjalme <kiendesylvia8@gmail.com>
2023-11-16 15:25:45 +0300

    removed white spaces

--- _printint.c
+++ _printint.c
@@ -10,34 +10,33 @@
 int _printint(va_list var_list)
 {
 	int numb = va_arg(var_list, int);
-	int counter_pow, last, temp_pow,digit, temp_numb, pow = 0;
+	int counter_pow, last, temp_pow, digit, temp_numb, pow = 0;
 
 	if (numb < 0)
 	{
-    	numb*=-1;
-    	_putchar(45);
+		numb * = -1;
+		_putchar(45);
 	}
 	temp_numb = numb;
- 	while (temp_numb > 9)
- 	{
-     	temp_numb/= 10;
-     	pow++;
- 	}
- 	temp_numb = numb;
- 	temp_pow = pow;
- 	counter_pow = pow;
- 	for (; counter_pow > 0; counter_pow--)
- 	{
-      	for (; temp_pow > 0; temp_pow--)
-    	{
-        	temp_numb/=10;
-    	}
-    	digit = temp_numb % 10;
-    	_putchar(digit + '0');
-    	temp_pow = pow -1;
-    	temp_numb = numb;
- 	}
-
+	while (temp_numb > 9)
+	{
+		temp_numb /= 10;
+		pow++;
+	}
+	temp_numb = numb;
+	temp_pow = pow;
+	counter_pow = pow;
+	for (; counter_pow > 0; counter_pow--)
+	{
+		for (; temp_pow > 0; temp_pow--)
+		{
+			temp_numb /= 10;
+		}
+		digit = temp_numb % 10;
+		_putchar(digit + '0');
+		temp_pow = pow - 1;
+		temp_numb = numb;
+	}
 	last = numb % 10;
 	_putchar(last + '0');
 	return (0);
